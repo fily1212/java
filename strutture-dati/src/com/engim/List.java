af6ae04bbb15872d68d349603fd6c05c7b49b741
Zhongli Filippo Hu <zhonglifilippo.hu@unito.it>
2021-02-26 12:06:18 +0100

    strutture dati - Compiti

--- strutture-dati/src/com/engim/List.java
+++ strutture-dati/src/com/engim/List.java
@@ -4,5 +4,20 @@ public abstract class List {
 
     public abstract List addLast(int x);
     public abstract void addLast2(int x);
+    public abstract List add(int x, int i);
 
+    // rimuove tutte le occorrenze di x
+    public abstract List remove(int x);
+
+    // rimuove l'elemento in posizione i
+    public abstract List removeAtIndex(int i);
+
+    // raddoppia tutti gli elementi della lista
+    public abstract List doppio();
+
+    // true se tutti gli elementi sono pari
+    public abstract boolean pari();
+
+    // true se la lista è ordinata
+    public abstract boolean ordinata(int next);
 }
