61740fca7c688d9c16208d8c08c3b8bd63f779c8
Zhongli Filippo Hu <zhonglifilippo.hu@unito.it>
2021-03-25 10:34:41 +0100

    Compito in classe

---
+++ compito-strutture-dati/src/soluzione/Nil.java
@@ -0,0 +1,44 @@
+package soluzione;
+
+public class Nil extends List {
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+
+    @Override
+    public List add(int x, int i) {
+        return new Node(x, this);
+    }
+
+    @Override
+    public List remove(int x) {
+        return this;
+    }
+
+
+
+    @Override
+    public int countOdd() {
+        return 0;
+    }
+
+
+    @Override
+    public List doubleValue() {
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Nil;
+    }
+
+    @Override
+    public List getNext(){
+        return this;
+    }
+
+}
